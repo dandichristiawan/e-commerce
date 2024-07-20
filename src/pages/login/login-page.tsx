01b2f2b021419d7c2e058b03756c134698684a39
siroj05 <rojudin123@gmail.com>
2024-07-20 17:52:17 +0700

    fix login page

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
 import { Login } from '@/features/login';
-import { MainLayout } from '@/layouts/main-layouts';
 
 export const LoginPage = () => {
   return (
     <>
-      <MainLayout>
-        <Login />
-      </MainLayout>
+      <Login />
     </>
   );
 };
